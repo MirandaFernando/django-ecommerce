8ac391eab5bf642f71b5810d610c523a408c74ed
Fernando <fernandomirandalolzeiro@gmail.com>
2021-03-16 14:57:13 -0300

    contruindo list view dos produtos para o usuário comum.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Product
+
+class ProductListView(ListView):
+    #traz todos os produtos do banco de dados sem filtrar nada 
+    queryset = Product.objects.all()
+    template_name = "products/list.html"
+    
+    
+
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, "products/list.html", context)
